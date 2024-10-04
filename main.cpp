cc4f8aa10b60406d3f32971c80ddce32d7a5121d
dragofiro <dragokin2024@gmail.com>
2024-10-04 14:49:53 -0400

    where do I commit

--- main.cpp
+++ main.cpp
@@ -12,8 +12,8 @@ int main(int argc, char* argv[]) {
         intList.push_back(temp);
     }
 
-    std::list<int>::iterator it = intList.begin();
-    while (it != intList.end()) {
+    std::list<int>::reverse_iterator it = intList.rbegin();
+    while (it != intList.rend()) {
         std::cout << *it << std::endl;
         it++;
     }
